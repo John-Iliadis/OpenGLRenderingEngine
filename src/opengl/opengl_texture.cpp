07c64c426854cb18cbc67dd4ce9e986b5f5d71dd
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-01-22 17:03:29 +1100

    added multisampled and cubemap textures

--- src/opengl/opengl_texture.cpp
+++ src/opengl/opengl_texture.cpp
@@ -21,9 +21,9 @@ GLenum toGLenum(TextureFormat format)
     }
 }
 
-GLenum toGLenum(TextureDataType type)
+GLenum toGLenum(TextureDataType dataType)
 {
-    switch (type)
+    switch (dataType)
     {
         case TextureDataType::UINT8: return GL_UNSIGNED_BYTE;
         case TextureDataType::FLOAT: return GL_FLOAT;
@@ -72,7 +72,7 @@ uint32_t calculateMipLevels(int32_t textureWidth, int32_t textureHeight)
     return glm::floor(glm::log2((float)glm::max(textureWidth, textureHeight))) + 1;
 }
 
-std::shared_ptr<uint8_t> loadImage(const std::string& imagePath, int32_t* width, int32_t* height, int requiredChannels)
+std::shared_ptr<uint8_t> loadImage(const std::string& imagePath, int32_t* width, int32_t* height, int32_t requiredChannels)
 {
     uint8_t* data = stbi_load(imagePath.c_str(), width, height, nullptr, requiredChannels);
     check(data, std::format("Failed to load {}", imagePath).c_str());
@@ -80,15 +80,15 @@ std::shared_ptr<uint8_t> loadImage(const std::string& imagePath, int32_t* width,
     return std::shared_ptr<uint8_t>(data, [] (uint8_t* d) { stbi_image_free(d); });
 }
 
-std::shared_ptr<float> loadImageHDR(const std::string& imagePath, int32_t* width, int32_t* height, int requiredChannels)
+std::shared_ptr<uint8_t> loadImageHDR(const std::string& imagePath, int32_t* width, int32_t* height, int32_t requiredChannels)
 {
     float* data = stbi_loadf(imagePath.c_str(), width, height, nullptr, requiredChannels);
     check(data, std::format("Failed to load {}", imagePath).c_str());
 
-    return std::shared_ptr<float>(data, [] (float* d) { stbi_image_free(d); });
+    return std::shared_ptr<uint8_t>((uint8_t*)data, [] (uint8_t* d) { stbi_image_free(d); });
 }
 
-int getRequiredChannels(TextureFormat format)
+int32_t getRequiredChannels(TextureFormat format)
 {
     switch (format)
     {
@@ -98,4 +98,252 @@ int getRequiredChannels(TextureFormat format)
         case TextureFormat::RGBA32F: return 4;
         default: assert(false);
     }
+}
+
+// -- OpenGLTexture -- //
+
+OpenGLTexture::OpenGLTexture()
+    : mRendererID()
+    , mSpecification()
+{
+}
+
+OpenGLTexture::OpenGLTexture(const TextureSpecification &spec)
+    : mRendererID()
+    , mSpecification(spec)
+{
+}
+
+OpenGLTexture::~OpenGLTexture()
+{
+    glDeleteTextures(1, &mRendererID);
+}
+
+OpenGLTexture::OpenGLTexture(OpenGLTexture &&other) noexcept
+{
+    std::swap(mRendererID, other.mRendererID);
+    std::swap(mSpecification, other.mSpecification);
+}
+
+OpenGLTexture &OpenGLTexture::operator=(OpenGLTexture &&other) noexcept
+{
+    if (this != &other)
+    {
+        glDeleteTextures(1, &mRendererID);
+
+        std::swap(mRendererID, other.mRendererID);
+        std::swap(mSpecification, other.mSpecification);
+    }
+
+    return *this;
+}
+
+void OpenGLTexture::bind(uint32_t unit)
+{
+    glBindTextureUnit(unit, mRendererID);
+}
+
+void OpenGLTexture::unbind(uint32_t unit)
+{
+    glBindTextureUnit(unit, 0);
+}
+
+uint32_t OpenGLTexture::id() const
+{
+    return mRendererID;
+}
+
+int32_t OpenGLTexture::width() const
+{
+    return mSpecification.width;
+}
+
+int32_t OpenGLTexture::height() const
+{
+    return mSpecification.height;
+}
+
+// -- OpenGLTexture2D -- //
+
+OpenGLTexture2D::OpenGLTexture2D(const TextureSpecification &spec)
+    : OpenGLTexture(spec)
+{
+    create();
+}
+
+OpenGLTexture2D::OpenGLTexture2D(const TextureSpecification &spec, const void *textureData)
+    : OpenGLTexture(spec)
+{
+    create();
+    uploadTextureData(textureData);
+}
+
+OpenGLTexture2D::OpenGLTexture2D(const TextureSpecification &spec, const std::string &texturePath)
+    : OpenGLTexture(spec)
+{
+    int32_t requiredChannels = getRequiredChannels(mSpecification.format);
+
+    std::shared_ptr<uint8_t> textureData;
+    if (spec.dataType == TextureDataType::FLOAT)
+        textureData = loadImageHDR(texturePath, &mSpecification.width, &mSpecification.height, requiredChannels);
+    else
+        textureData = loadImage(texturePath, &mSpecification.width, &mSpecification.height, requiredChannels);
+
+    create();
+    uploadTextureData(textureData.get());
+}
+
+void OpenGLTexture2D::resize(int32_t width, int32_t height)
+{
+    mSpecification.width = width;
+    mSpecification.height = height;
+
+    glDeleteTextures(1, &mRendererID);
+
+    create();
+}
+
+void OpenGLTexture2D::create()
+{
+    glCreateTextures(GL_TEXTURE_2D, 1, &mRendererID);
+    glTextureParameteri(mRendererID, GL_TEXTURE_WRAP_S, toGLenum(mSpecification.wrapMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_WRAP_T, toGLenum(mSpecification.wrapMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_MIN_FILTER, toGLenumMinFilter(mSpecification.filterMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_MAG_FILTER, toGLenumMinFilter(mSpecification.filterMode));
+
+    if (mSpecification. filterMode == TextureFilter::Anisotropic)
+    {
+        float maxAnisotropy = 0.f;
+        glGetTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, &maxAnisotropy);
+        glTextureParameterf(mRendererID, GL_TEXTURE_MAX_ANISOTROPY, glm::max(16.f, maxAnisotropy));
+    }
+
+    uint32_t mipLevels = 0;
+    if (mSpecification.generateMipMaps)
+        mipLevels = calculateMipLevels(mSpecification.width, mSpecification.height);
+
+    glTextureStorage2D(mRendererID,
+                       mipLevels,
+                       toGLenum(mSpecification.format),
+                       mSpecification.width,
+                       mSpecification.height);
+}
+
+void OpenGLTexture2D::uploadTextureData(const void *textureData)
+{
+    glTextureSubImage2D(mRendererID,
+                        0,
+                        0, 0,
+                        mSpecification.width,
+                        mSpecification.height,
+                        toGLenum(mSpecification.format),
+                        toGLenum(mSpecification.dataType),
+                        textureData);
+}
+
+// -- OpenGLTexture2DMultisample -- //
+
+OpenGLTexture2DMultisample::OpenGLTexture2DMultisample(const TextureSpecification &spec, int32_t sampleCount)
+    : OpenGLTexture(spec)
+{
+    int32_t maxSamples;
+    glGetIntegerv(GL_MAX_SAMPLES, &maxSamples);
+    mSampleCount = glm::max(sampleCount, maxSamples);
+
+    create();
+}
+
+void OpenGLTexture2DMultisample::resize(int32_t width, int32_t height)
+{
+    mSpecification.width = width;
+    mSpecification.height = height;
+
+    glDeleteTextures(1, &mRendererID);
+
+    create();
+}
+
+void OpenGLTexture2DMultisample::create()
+{
+    glCreateTextures(GL_TEXTURE_2D_MULTISAMPLE, 1, &mRendererID);
+    glTextureStorage2DMultisample(mRendererID,
+                                  mSampleCount,
+                                  toGLenum(mSpecification.format),
+                                  mSpecification.width,
+                                  mSpecification.height,
+                                  GL_FALSE);
+}
+
+// -- OpenGLTextureCube -- //
+
+OpenGLTextureCube::OpenGLTextureCube(const TextureSpecification &spec)
+    : OpenGLTexture(spec)
+{
+    create();
+}
+
+OpenGLTextureCube::OpenGLTextureCube(const TextureSpecification &spec, const void **textureData)
+    : OpenGLTextureCube(spec)
+{
+    create();
+    for (int32_t i = 0; i < 6; ++i)
+        uploadTextureData(i, textureData[i]);
+}
+
+OpenGLTextureCube::OpenGLTextureCube(const TextureSpecification &spec, const std::array<std::string, 6> &texturePaths)
+    : OpenGLTexture(spec)
+{
+    create();
+
+    int32_t requiredChannels = getRequiredChannels(mSpecification.format);
+    for (uint32_t i = 0; i < 6; ++i)
+    {
+        std::shared_ptr<uint8_t> textureData;
+        if (spec.dataType == TextureDataType::FLOAT)
+            textureData = loadImageHDR(texturePaths.at(i), &mSpecification.width, &mSpecification.height, requiredChannels);
+        else
+            textureData = loadImage(texturePaths.at(i), &mSpecification.width, &mSpecification.height, requiredChannels);
+
+        uploadTextureData(i, textureData.get());
+    }
+}
+
+void OpenGLTextureCube::create()
+{
+    glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &mRendererID);
+    glTextureParameteri(mRendererID, GL_TEXTURE_WRAP_S, toGLenum(mSpecification.wrapMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_WRAP_T, toGLenum(mSpecification.wrapMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_WRAP_R, toGLenum(mSpecification.wrapMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_MIN_FILTER, toGLenumMinFilter(mSpecification.filterMode));
+    glTextureParameteri(mRendererID, GL_TEXTURE_MAG_FILTER, toGLenumMinFilter(mSpecification.filterMode));
+
+    if (mSpecification. filterMode == TextureFilter::Anisotropic)
+    {
+        float maxAnisotropy = 0.f;
+        glGetTexParameterfv(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAX_ANISOTROPY, &maxAnisotropy);
+        glTextureParameterf(mRendererID, GL_TEXTURE_MAX_ANISOTROPY, glm::max(16.f, maxAnisotropy));
+    }
+
+    uint32_t mipLevels = 0;
+    if (mSpecification.generateMipMaps)
+        mipLevels = calculateMipLevels(mSpecification.width, mSpecification.height);
+
+    glTextureStorage2D(mRendererID,
+                       mipLevels,
+                       toGLenum(mSpecification.format),
+                       mSpecification.width,
+                       mSpecification.height);
+}
+
+void OpenGLTextureCube::uploadTextureData(int32_t faceIndex, const void* textureData)
+{
+    glTextureSubImage3D(mRendererID,
+                        0,
+                        0, 0, faceIndex,
+                        mSpecification.width,
+                        mSpecification.height,
+                        1,
+                        toGLenum(mSpecification.format),
+                        toGLenum(mSpecification.dataType),
+                        textureData);
 }
--- src/opengl/opengl_texture.hpp
+++ src/opengl/opengl_texture.hpp
@@ -53,15 +53,81 @@ struct TextureSpecification
 };
 
 GLenum toGLenum(TextureFormat format);
-GLenum toGLenum(TextureDataType type);
+GLenum toGLenum(TextureDataType dataType);
 GLenum toGLenum(TextureWrap wrapMode);
 GLenum toGLenumMinFilter(TextureFilter filterMode);
 GLenum toGLenumMagFilter(TextureFilter filterMode);
 uint32_t calculateMipLevels(int32_t textureWidth, int32_t textureHeight);
 
-std::shared_ptr<uint8_t> loadImage(const std::string& imagePath, int32_t* width, int32_t* height, int requiredChannels);
-std::shared_ptr<float> loadImageHDR(const std::string& imagePath, int32_t* width, int32_t* height, int requiredChannels);
+std::shared_ptr<uint8_t> loadImage(const std::string& imagePath, int32_t* width, int32_t* height, int32_t requiredChannels);
+std::shared_ptr<uint8_t> loadImageHDR(const std::string& imagePath, int32_t* width, int32_t* height, int32_t requiredChannels);
 
-int getRequiredChannels(TextureFormat format);
+int32_t getRequiredChannels(TextureFormat format);
+
+class OpenGLTexture
+{
+public:
+    OpenGLTexture();
+    OpenGLTexture(const TextureSpecification& spec);
+    virtual ~OpenGLTexture();
+
+    OpenGLTexture(const OpenGLTexture&) = delete;
+    OpenGLTexture& operator=(const OpenGLTexture&) = delete;
+
+    OpenGLTexture(OpenGLTexture&& other) noexcept;
+    OpenGLTexture& operator=(OpenGLTexture&& other) noexcept;
+
+    void bind(uint32_t unit);
+    void unbind(uint32_t unit);
+
+    uint32_t id() const;
+    int32_t width() const;
+    int32_t height() const;
+
+protected:
+    uint32_t mRendererID;
+    TextureSpecification mSpecification;
+};
+
+class OpenGLTexture2D : public OpenGLTexture
+{
+public:
+    OpenGLTexture2D() = default;
+    OpenGLTexture2D(const TextureSpecification& spec);
+    OpenGLTexture2D(const TextureSpecification& spec, const void* textureData);
+    OpenGLTexture2D(const TextureSpecification& spec, const std::string& texturePath);
+
+    void resize(int32_t width, int32_t height);
+
+private:
+    void create();
+    void uploadTextureData(const void* textureData);
+};
+
+class OpenGLTexture2DMultisample : public OpenGLTexture
+{
+public:
+    OpenGLTexture2DMultisample() = default;
+    OpenGLTexture2DMultisample(const TextureSpecification& spec, int32_t sampleCount);
+
+    void resize(int32_t width, int32_t height);
+
+private:
+    void create();
+    int32_t mSampleCount;
+};
+
+class OpenGLTextureCube : public OpenGLTexture
+{
+public:
+    OpenGLTextureCube() = default;
+    OpenGLTextureCube(const TextureSpecification& spec);
+    OpenGLTextureCube(const TextureSpecification& spec, const void** textureData);
+    OpenGLTextureCube(const TextureSpecification& spec, const std::array<std::string, 6>& texturePaths);
+
+private:
+    void create();
+    void uploadTextureData(int32_t faceIndex, const void* textureData);
+};
 
 #endif //OPENGLRENDERINGENGINE_OPENGL_TEXTURE_HPP
