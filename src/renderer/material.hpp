364234b4dd0f017d85c31db8d460bb22ab42efca
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2025-01-28 08:28:09 +1100

    update material

--- src/renderer/material.hpp
+++ src/renderer/material.hpp
@@ -9,37 +9,41 @@
 
 enum class MaterialTextureType : uint32_t
 {
-    Albedo = 0,
-    Specular,
-    Roughness,
-    Metallic,
+    BaseColor = 0,
+    MetallicRoughness,
     Normal,
-    Displacement,
     Ao,
-    Emission
+    Emission,
+    SpecularGlossiness,
+    Displacement
 };
 
-enum class Workflow : uint32_t
-{
-    Specular = 0,
-    Metallic = 1
-};
+#define METALLIC_WORKFLOW = 0
+#define SPECULAR_WORKFLOW = 1
+
+// - metallic workflow uses the metallic roughness map
+// - specular workflow uses the specular glossiness/specular map
+// - for the specular workflow, the base color map will be the diffuse map
 
 struct Material
 {
-    Workflow workflow;
-    uint32_t albedoMapIndex;
-    uint32_t specularMapIndex;
-    uint32_t roughnessMapIndex;
-    uint32_t metallicMapIndex;
+    uint32_t workflow;
+    uint32_t baseColorMapIndex;
+    uint32_t metallicRoughnessMapIndex;
     uint32_t normalMapIndex;
-    uint32_t displacementMapIndex;
     uint32_t aoMapIndex;
     uint32_t emissionMapIndex;
-    glm::vec4 albedoColor;
-    glm::vec4 emissionColor;
-    glm::vec2 tiling;
-    glm::vec2 offset;
+    uint32_t specularGlossinessMapIndex;
+    uint32_t displacementMapIndex;
+    glm::vec4 baseColorFactor = glm::vec4(1.f);
+    glm::vec4 emissionFactor = glm::vec4(0.f);
+    glm::vec4 specularGlossinessFactor = glm::vec4(1.f);
+    float metallicFactor = 1.f;
+    float roughnessFactor = 1.f;
+    float occlusionFactor = 1.f;
+    float specularFactor = 1.f;
+    glm::vec2 tiling = glm::vec2(1.f);
+    glm::vec2 offset = glm::vec2(0.f);
 };
 
 #endif //OPENGLRENDERINGENGINE_MATERIAL_HPP
